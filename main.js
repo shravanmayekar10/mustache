4bfd3c34689d8c9c9bbac10889074afe2393568b
shravanmayekar10 <93005855+shravanmayekar10@users.noreply.github.com>
2022-08-09 12:14:38 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+noseX=0;
+noseY=0;
+function preload(){
+    clown_nose=loadImage("https://i.postimg.cc/xT7NryS4/il-fullxfull-2688313853-4b66.webp");
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+ video.hide();
+
+ poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log('poseNet Is Initialized');
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+console.log(results);
+noseX = results[0].pose.nose.x - 15;
+ noseY = results[0].pose.nose.y - 15;
+    }
+}
+function draw() {
+    image(video, 0, 0, 300, 300);
+image(clown_nose,noseX,noseY,30,30,30);
+}
+function take_snapshot(){
+    save('myFilterImage.png');
+
+}
